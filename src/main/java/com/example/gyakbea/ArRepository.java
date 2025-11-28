c38de6dfc95dd46c9e0bbd9f847f4da55b05ba13
Muskó Milán <milanka1219@gmail.com>
2025-11-28 13:20:06 +0100

    Adatbázis menüpont teljes kialakítása

--- src/main/java/com/example/gyakbea/ArRepository.java
+++ src/main/java/com/example/gyakbea/ArRepository.java
@@ -1,3 +1,9 @@
 package com.example.gyakbea;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface ArRepository extends JpaRepository<Ar, Long> {}
+import java.util.List;
+
+public interface ArRepository extends JpaRepository<Ar, Long> {
+    // SutiID alapján keressük az árat
+    List<Ar> findBySutiid(Long sutiid);
+}
